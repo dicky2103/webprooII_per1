c4875a0f4058c26026f479ca4bd6df5d8005b214
dicky azhari hernuwan <hernuwandicky@gmail.com>
2022-06-17 23:06:11 +0700

    tugas1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Nama Saya Dicky Azhari Hernuwan <br>";
+        echo "Kelas 19.3B.24 <br>";
+        echo "Jurusan Sistem Informasi <br>";
+    }
+    
+}
